18d449e8d228966e22354d9278d3f8329bd71d36
aiolar <fadenipraisejah@gmail.com>
2022-03-10 17:25:51 -0800

    using printf to output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints
+ * with proper grammer, but the outome is a piece of art,
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
