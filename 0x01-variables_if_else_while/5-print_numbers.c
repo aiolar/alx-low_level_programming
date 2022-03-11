1209fb639043bb7a681cb31976a2e866480bc053
aiolar <fadenipraisejah@gmail.com>
2022-03-11 05:09:32 -0800

    script5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0 ; digit < 10 ; digit++)
+	{
+		printf("%d",digit);
+	}
+	return (0);
+}
