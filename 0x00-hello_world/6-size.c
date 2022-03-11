1de88b7f97b7f0c8336d54d221fa9e99bd0380cb
aiolar <fadenipraisejah@gmail.com>
2022-03-10 17:44:10 -0800

    script 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing sizes of types
+ *
+ * Description: This program prints the size of
+ * vaious types on the computer it is compiled and run on
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %c byte(s)");
+	printf("Size of an int: %d byte(s)");
+	printf("Size of a long int: %ld byte(s)");
+	printf("Size of a long long int: %lld byte(s)");
+	printf("Size of a float: %f byte(s)");
+	return (0);
+}
