eef91b74c348734d0630c289b5360118cac8437b
aiolar <fadenipraisejah@gmail.com>
2022-03-11 09:33:19 -0800

    script8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alp;
+
+	for (alp = 0 ; alp <= 16 ; alp++)
+	{
+		putchar(alp + '0');
+		if (alp > 9)
+		{
+			putchar("%x");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
