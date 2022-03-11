e353c0fd4f4ea58e5ab43e4edcb3e895eefa9a3d
aiolar <fadenipraisejah@gmail.com>
2022-03-11 04:11:35 -0800

    script1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 	printf("Last digit of %d is %d", n, last_no);
 	if (last_no > 5)
 	{
-		printf("and is greater than 5\n");
+		printf(" and is greater than 5\n");
 	}
 	else if (last_no == 0)
 	{
-		printf("and is 0\n");
+		printf(" and is 0\n");
 	}
 	else if ((last_no < 6) && (last_no != 0))
 	{
-		printf("and is less than 6 and not 0\n");
+		printf(" and is less than 6 and not 0\n");
 	}
 	return (0);
 }
