a68c13ac52cfe2d7d29cc81612ae4c6d46635ef8
aiolar <fadenipraisejah@gmail.com>
2022-03-11 05:19:08 -0800

    script6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0 ; digit < 10 ; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
