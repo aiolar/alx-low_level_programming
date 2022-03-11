cb1db9c2fac4904e791ab75434138dd5d2b91a1d
aiolar <fadenipraisejah@gmail.com>
2022-03-10 19:05:26 -0800

    script7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints
+ * a pece of text to the standard error
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2016-10-19\n");
+	return (1);
+}
