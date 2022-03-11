840bb2704d487f006b18ac1773e294e235343a27
aiolar <fadenipraisejah@gmail.com>
2022-03-11 13:39:21 -0800

    script9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alp;
+
+	for (alp = 0 ; alp < 10 ; alp++)
+	{
+		putchar(alp + '0');
+	}
+	putchar(', ');
+	putchar('\n');
+	return (0);
+}
