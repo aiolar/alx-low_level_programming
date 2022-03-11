2079c07cade550379a13c8335fa7270d71c245a1
aiolar <fadenipraisejah@gmail.com>
2022-03-10 16:00:43 -0800

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints "Programming is like building a  multilingual  puzzle"
+ *
+ * Retun: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
