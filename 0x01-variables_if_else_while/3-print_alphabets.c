62bcbcdf1dfaa703865c2e40317c1387cbba71a3
aiolar <fadenipraisejah@gmail.com>
2022-03-11 04:36:09 -0800

    script3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a' ; alp <= 'z' ; alp++)
+	{
+		putchar(alp);
+	}
+	for (alp = 'A' ; alp <= 'Z' ; alp++)
+	{
+		putchar(alp);
+	}
+	return (0);
+}
