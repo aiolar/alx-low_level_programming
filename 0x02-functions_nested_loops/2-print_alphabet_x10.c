8a043015b15247ff0b081a2f336f27e921a95447
aiolar <fadenipraisejah@gmail.com>
2022-03-15 14:53:43 -0700

    script2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i;
+	int counter = 0;
+
+	while (counter < 10)
+	{
+		for (i = 0 ; i < 26 ; i++)
+			{
+				_putchar(c + i);
+			}
+			_putchar('\n');
+			counter++;
+	}
+}
