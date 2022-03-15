5c5f00dc3d3d25a43e861aa2f144758c4ed2414b
aiolar <fadenipraisejah@gmail.com>
2022-03-15 14:28:14 -0700

    script1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,16 +8,12 @@
 
 void print_alphabet(void)
 {
-    int i;
-    for (i='a' ; i<='z' ; i++)
-    {
-	    _putchar(i);
-    }
-    _putchar('\n');
-}
+       char c = 'a';
+       int i;
 
-int main(void)
-{
-	print_alphabet();
-	return (0);
+       for (i = 0 ; i < 26 ; i++)
+       {
+	       _putchar(c + i);
+       }
+       _putchar('\n');
 }
