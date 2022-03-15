2998e0d2d48469e96784e32894968a0e84203b52
aiolar <fadenipraisejah@gmail.com>
2022-03-15 15:54:47 -0700

    script6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- *  * _abs - compute the absolute value of an integer
- *   * @n: int type number
- *    * Return: absolute value of @n
- *     */
+ * _abs - compute the absolute value of an integer
+ * 
+ * @n: int type number
+ * Return: absolute value of @n
+ *
+ * /
 
 int _abs(int n)
 {
