3cf27b3a62ffdb11832ab93c46f3aee9f57fd1c4
aiolar <fadenipraisejah@gmail.com>
2022-03-15 13:22:48 -0700

    script1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ * main - prints alphabet
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+    int i;
+    for (i='a' ; i<='z' ; i++)
+    {
+	    _putchar(i);
+    }
+    _putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
