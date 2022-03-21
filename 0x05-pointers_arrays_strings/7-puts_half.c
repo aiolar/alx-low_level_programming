1eee641eda933d941b52b4e581b0b029a859af1d
aiolar <fadenipraisejah@gmail.com>
2022-03-21 12:08:10 -0700

    task 7 correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,16 +11,16 @@ void puts_half(char *str)
 {
 	int i;
 	int length;
-	int odd_length;
-	
-	for (i = 0; str[i] != '\0'; ++i);
+	int n;
 
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+	}
 	length = i;
-	odd_length = (length - 1) / 2;
-
+	n = (length - 1) / 2;
 	if (length % 2 != 0)
 	{
-		for (i = odd_length; str[i] != '\0'; ++i)
+		for (i = n; str[i] != '\0'; ++i)
 		{
 			_putchar(str[i]);
 		}
