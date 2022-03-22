048e2edb809e6454d842e3cacfeae46862db91ee
aiolar <fadenipraisejah@gmail.com>
2022-03-21 19:45:16 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,34 +9,23 @@
 
 void rev(char *s)
 {
-	    int i, max, half;
-	        char first, last;
-		    
-		    i=0;
-		        while (s[i] != '\0')
-				    {
-					            i++;
-						        }
-			    printf("%d\n", i);
-			        max = i - 1;
-				    printf("%d\n", max);
-				        half = max / 2;
-					    printf("%d\n", half);
-					        while (half >=0)
-							    {
-								            first = s[max - half];
-									           // printf("%c", first);
-										   //         printf("%c, %d\n", first, (max-half));
-										   //                 last = s[half];
-										   //                        // printf("%c", last);
-										   //                                printf("%c, %d\n", last, half);
-										   //                                        s[half] = first;
-										   //                                               // printf("%c", s[half]);
-										   //                                                       printf("%c, %d\n", s[half], half);
-										   //                                                               s[max - half] = last;
-										   //                                                                      // printf("%c", s[max - half]);
-										   //                                                                              printf("%c, %d", s[max - half], (max-half));
-										   //                                                                                      half--;
-										   //                                                                                              printf("\n");
-										   //                                                                                                  }
-										   //                                                                                                  }
+	int i, max, half;
+	char first, last;
+	
+	i=0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	printf("%d\n", i);
+	max = i - 1
+	half = max / 2;
+	while (half >=0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
+	}
+}
