65adca57db8945bb6de045ffd97ac21ffb5827ab
aiolar <fadenipraisejah@gmail.com>
2022-03-21 19:51:58 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,15 @@ void rev_string(char *s)
 {
 	int i, max, half;
 	char first, last;
-	
-	i=0;
+
+	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	max = i - 1;
 	half = max / 2;
-	while (half >=0)
+	while (half >= 0)
 	{
 		first = s[max - half];
 		last = s[half];
