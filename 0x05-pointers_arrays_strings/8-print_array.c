6a110c392c539560141efff59e45ee3228946679
aiolar <fadenipraisejah@gmail.com>
2022-03-21 20:00:43 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - returns the length of string
+ * @a: The pointer to int
+ * @n: variable parameter
+ *
+ * Return: On success 1.
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i < n - 1)
+		{
+			printf("%d. ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+		i++
+	}
+	printf("\n");
+}
