cfb0920eeccab7e0b4a801d498b49e6949b6796e
aiolar <fadenipraisejah@gmail.com>
2022-03-21 19:37:46 -0700

    test5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,36 @@
  * Return: On success 1.
  */
 
-void rev_string(char *s)
+void rev(char *s)
 {
-	int i = 0;
-
-	while (s[i])
-		i++;
-	while (i--)
-	{
-		*s[i];
-	}
-}
+	    int i, max, half;
+	        char first, last;
+		    
+		    i=0;
+		        while (s[i] != '\0')
+				    {
+					            i++;
+						        }
+			    printf("%d\n", i);
+			        max = i - 1;
+				    printf("%d\n", max);
+				        half = max / 2;
+					    printf("%d\n", half);
+					        while (half >=0)
+							    {
+								            first = s[max - half];
+									           // printf("%c", first);
+										   //         printf("%c, %d\n", first, (max-half));
+										   //                 last = s[half];
+										   //                        // printf("%c", last);
+										   //                                printf("%c, %d\n", last, half);
+										   //                                        s[half] = first;
+										   //                                               // printf("%c", s[half]);
+										   //                                                       printf("%c, %d\n", s[half], half);
+										   //                                                               s[max - half] = last;
+										   //                                                                      // printf("%c", s[max - half]);
+										   //                                                                              printf("%c, %d", s[max - half], (max-half));
+										   //                                                                                      half--;
+										   //                                                                                              printf("\n");
+										   //                                                                                                  }
+										   //                                                                                                  }
