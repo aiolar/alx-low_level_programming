bddc05b977442b8a735f9f04e38c8e600fc38793
aiolar <fadenipraisejah@gmail.com>
2022-03-21 18:42:02 -0700

    test7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,7 +21,7 @@ void puts_half(char *str)
 	n = (length - 1) / 2;
 	if (length % 2 != 0)
 	{
-		for (i = n - 1; str[i] != '\0'; i++)
+		for (i = n; str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
