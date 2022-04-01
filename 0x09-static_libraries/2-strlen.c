70a67268c0d6ce6b4b761ca89ac40ccab7f28f8d
aiolar <fadenipraisejah@gmail.com>
2022-04-01 13:58:24 -0700

    static lib

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of string
+ * @s: The pointer to string
+ *
+ * Return: On success 1.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+	}
+	return (i);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by new line
+ * @str: The pointer to string
+ *
+ * Return: On success 1.
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
